03f86168722c5a3667d7eda7ba611f1a66d7a8f1
yash <yashpendal18@gnu.ac.in>
2023-08-11 15:56:30 +0530

    change19

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Welcome to the Allianz Private Cloud from India')
+  response.send('Welcome to the Allianz Private Cloud')
 })
 
 app.listen(app.get('port'), function() {
